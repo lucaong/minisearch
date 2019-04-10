35bfd93d0e2b75d97f7362681aea1e396eb133ec
Luca Ongaro <mail@lucaongaro.eu>
2019-04-10 12:28:18 +0200

    better code formatting

--- src/SearchableMap/fuzzySearch.js
+++ src/SearchableMap/fuzzySearch.js
@@ -32,23 +32,36 @@ export const withinDistance = function (a, b, maxDistance, i, edit) {
   const stack = [{ distance: 0, ia: i, ib: 0, edit }]
   const mem = []
   const results = []
+
   while (stack.length > 0) {
     const { distance, ia, ib, edit } = stack.pop()
+
     mem[ia] = mem[ia] || []
     if (mem[ia][ib] && mem[ia][ib] <= distance) { continue }
     mem[ia][ib] = distance
+
     if (ib === b.length) {
       results.push({ distance, i: ia, edit })
       continue
     }
+
     if (a[ia] === b[ib]) {
       stack.push({ distance, ia: ia + 1, ib: ib + 1, edit: NONE })
     } else {
       if (distance >= maxDistance) { continue }
-      if (edit !== ADD) { stack.push({ distance: distance + 1, ia, ib: ib + 1, edit: DELETE }) }
+
+      if (edit !== ADD) {
+        stack.push({ distance: distance + 1, ia, ib: ib + 1, edit: DELETE })
+      }
+
       if (ia < a.length) {
-        if (edit !== DELETE) { stack.push({ distance: distance + 1, ia: ia + 1, ib, edit: ADD }) }
-        if (edit !== DELETE && edit !== ADD) { stack.push({ distance: distance + 1, ia: ia + 1, ib: ib + 1, edit: CHANGE }) }
+        if (edit !== DELETE) {
+          stack.push({ distance: distance + 1, ia: ia + 1, ib, edit: ADD })
+        }
+
+        if (edit !== DELETE && edit !== ADD) {
+          stack.push({ distance: distance + 1, ia: ia + 1, ib: ib + 1, edit: CHANGE })
+        }
       }
     }
   }
