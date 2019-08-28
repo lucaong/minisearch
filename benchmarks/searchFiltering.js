19664eb3a30ed8c22711e76fffd23f62d7f048a0
Luca Ongaro <mail@lucaongaro.eu>
2019-08-28 17:54:56 +0200

    refactoring and performance optimizations

---
+++ benchmarks/searchFiltering.js
@@ -0,0 +1,12 @@
+import Benchmark from 'benchmark'
+import { miniSearch } from './divinaCommedia.js'
+
+const suite = new Benchmark.Suite('Search filtering')
+suite.add('MiniSearch#search("virtu", { filter: ... })', () => {
+  miniSearch.search('virtu', {
+    prefix: true,
+    filter: ({ id }) => id.startsWith('Inf')
+  })
+})
+
+export default suite
