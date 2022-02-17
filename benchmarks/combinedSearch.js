3713c0eef7873daf8efeaac736fd4e39183b7c55
Luca Ongaro <mail@lucaongaro.eu>
2022-02-17 22:40:27 +0100

    Slightly improved benchmark

--- benchmarks/combinedSearch.js
+++ benchmarks/combinedSearch.js
@@ -2,11 +2,17 @@ const Benchmark = require('benchmark')
 import { miniSearch as ms } from './divinaCommedia.js'
 
 const suite = new Benchmark.Suite('Combined search')
-suite.add('MiniSearch#search("virtute e conoscienza")', () => {
-  ms.search('virtute e conoscienza', {
+suite.add('MiniSearch#search("virtute conoscienza", { fuzzy: 0.2, prefix: true })', () => {
+  ms.search('virtute conoscienza', {
+    fuzzy: 0.2,
+    prefix: true
+  })
+}).add('MiniSearch#search("virtu", { fuzzy: 0.2, prefix: true })', () => {
+  ms.search('virtu', {
     fuzzy: 0.2,
     prefix: true
   })
 })
+
 
 export default suite
