44be7365ee79a007e6a80cf1e1a756beaa7fff04
Rolf Timmermans <rolftimmermans@voormedia.com>
2022-02-17 12:10:53 +0100

    Add failing test for a fuzzy+prefix match scoring higher than an exact match.

--- src/MiniSearch.test.js
+++ src/MiniSearch.test.js
@@ -581,6 +581,21 @@ describe('MiniSearch', () => {
       expect(fuzzyLast[0].score).toEqual(exact[0].score)
     })
 
+    it('assigns weight lower than exact match to a match that is both a prefix and fuzzy match', () => {
+      const ms = new MiniSearch({ fields: ['text'] })
+      const documents = [
+        { id: 1, text: 'Poi che la gente poverella crebbe' },
+        { id: 2, text: 'Deus, venerunt gentes' }
+      ]
+      ms.addAll(documents)
+      expect(ms.documentCount).toEqual(documents.length)
+
+      const exact = ms.search('gente')
+      const combined = ms.search('gente', { fuzzy: true, prefix: true })
+      expect(combined.map(({ id }) => id)).toEqual([1, 2])
+      expect(combined[0].score).toEqual(exact[0].score)
+    })
+
     it('accepts a function to compute fuzzy and prefix options from term', () => {
       const fuzzy = jest.fn(term => term.length > 4 ? 2 : false)
       const prefix = jest.fn(term => term.length > 4)
