2836328724292fb866880735b5558070d6d45503
Rolf Timmermans <rolftimmermans@voormedia.com>
2022-02-21 15:09:40 +0100

    Improve TypeScript types.

--- src/SearchableMap/types.ts
+++ src/SearchableMap/types.ts
@@ -1,4 +1,12 @@
-export type RadixTree<T> = Map<string, T | RadixTree<T>>;
+export type LeafType = '' & { readonly __tag: unique symbol }
+
+export interface RadixTree<T> extends Map<string, T | RadixTree<T>> {
+  get(key: LeafType): T | undefined
+  get(key: string): RadixTree<T> | undefined
+
+  set(key: LeafType, value: T): this
+  set(key: string, value: RadixTree<T>): this
+}
 
 export type Entry<T> = [string, T]
 
