4a358c896a5166b7acee8b0cd6d84624b76e3e97
Rolf Timmermans <rolftimmermans@voormedia.com>
2022-02-21 15:14:28 +0100

    Add comment.

--- src/SearchableMap/types.ts
+++ src/SearchableMap/types.ts
@@ -1,6 +1,9 @@
 export type LeafType = '' & { readonly __tag: unique symbol }
 
 export interface RadixTree<T> extends Map<string, T | RadixTree<T>> {
+  // Distinguish between an empty string indicating a leaf node and a non-empty
+  // string indicating a subtree. Overriding these types avoids a lot of type
+  // assertions elsewhere in the code.
   get(key: LeafType): T | undefined
   get(key: string): RadixTree<T> | undefined
 
